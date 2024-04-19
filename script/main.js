25c80d038b57ca30515b79a924cf99cc95073f8b
odinalx <odin.alexandre26@gmail.com>
2024-04-19 19:52:48 +0200

    fix(build): add sphere to build

--- script/main.js
+++ script/main.js
@@ -202,3 +202,13 @@ scene.add(spherePoints);
 }
 
 init();
+
+document.addEventListener('DOMContentLoaded', function () {
+    const burgerButton = document.querySelector('.burgerbtn');
+    const burgerMenu = document.querySelector('.burger');
+
+    burgerButton.addEventListener('click', function () {
+        burgerMenu.classList.toggle('active');
+        document.body.classList.toggle('no-scroll');
+    });
+});
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        sphere: resolve(__dirname, 'sphere.html'),
+      },
+    },
+  },
+})
